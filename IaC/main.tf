20b149ca6518f0a24f06225a08c43c8f1cacaef6
Gabo <Gabovillayzan@users.noreply.github.com>
2020-08-01 03:45:56 -0500

    Add files via upload

---
+++ IaC/main.tf
@@ -0,0 +1,32 @@
+#version 1.0
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "myBucket" {
+  bucket = "flugel-bucket"
+  force_destroy = true
+  acl    = "public-read"
+  provisioner "local-exec" {
+    command = <<EOT
+    mkdir out
+    Get-Date > out/test1.txt
+    Get-Date > out/test2.txt
+    EOT
+    interpreter = ["PowerShell", "-Command"]
+  }
+  provisioner "local-exec" {
+    command = <<EOT
+    aws s3 cp D:\\Projects\\Terraform-Flugel\\IaC\\out\  s3://${aws_s3_bucket.myBucket.id} --acl public-read --recursive --exclude "*" --include "*.txt"
+    EOT
+  }
+  tags = {
+    Name        = "flugel-bucket"
+    Environment = "Dev"
+  }
+}
+
+output "BucketID" {
+  value = "${aws_s3_bucket.myBucket.id}"
+  
+}
